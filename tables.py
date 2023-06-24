092d78efe9ed97c0e0599a6b8202933702152469
Ashish Dewangan <ashishdewangan.learn@gmail.com>
2023-06-24 11:56:12 +0530

    Adding for loop for iteration

--- tables.py
+++ tables.py
@@ -1,13 +1,24 @@
 import random
 
-n1 = random.randrange(1,10)
-n2 = random.randrange(1,10)
-ans = input(str(n1) + " X " + str(n2) + " = ")
+correct = 0
+incorrect = 0
+total = 10
 
-corr_ans = n1 * n2
+for i in range(total):
+  n1 = random.randrange(1,10)
+  n2 = random.randrange(1,10)
+  ans = input(str(n1) + " X " + str(n2) + " = ")
 
-if int(ans) == corr_ans:
-  print('Awesome')
-else:
-  print('Aah ... miseed. Correct answer is: ' + str(corr_ans))
+  corr_ans = n1 * n2
+
+  if int(ans) == corr_ans:
+    print('Awesome')
+    correct+=1
+  else:
+    print('Aah ... miseed. Correct answer is: ' + str(corr_ans))
+    incorrect+=1
+
+print('You got %d correct out of %d' % (correct, total))
+
+
 
